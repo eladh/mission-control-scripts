fff3be47090e511047592756d5c0ec6f2b7d7c6d
jfmc <jfmc@mission-control-5c94bf5b96-58fsb>
2019-03-05 05:08:32 +0000

    File changed by admin

---
+++ star-replication.groovy
@@ -0,0 +1,28 @@
+sourceArti = userInput (
+        type : "ARTIFACTORY",
+        description : "Please provide the source Artifactory you want to host the replication",
+)
+
+targetArti = userInput (
+        type : "ARTIFACTORY",
+        description : "Please provide the target Artifactory you want to host the replication",
+
+)
+
+targetRepo = userInput (
+        type : "STRING",
+        description : "Repository Key",
+)
+
+artifactory(sourceArti.name) {
+    repository("npm-local", "npm-virtual") {
+        starPull(targetArti.name){
+            socketTimeoutMillis 60000
+            enableEventReplication true
+            cronExp '0 0/9 14 * * ?'
+            syncDeletes false
+            syncProperties false
+            syncStatistics false
+        }
+    }
+}
