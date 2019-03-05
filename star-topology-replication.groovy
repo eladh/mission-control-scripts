3493b7a2269b624e6ac1602bcafb2a8f686d88b0
jfmc <jfmc@mission-control-5c94bf5b96-58fsb>
2019-03-05 03:35:44 +0000

    File changed by admin

--- star-topology-replication.groovy
+++ star-topology-replication.groovy
@@ -1,13 +1,26 @@
 //Artifactory
-art1 = userInput (
+sourceArti = userInput (
+        type : "ARTIFACTORY",
+        description : "Please provide the source Artifactory you want to host the replication",
+)
+
+targetArti = userInput (
         type : "ARTIFACTORY",
         description : "Please provide the target Artifactory you want to host the replication",
+
+)
+
+repo = userInput (
+        type : "STRING",
+        description : "Repository Key",
 )
 
 
-artifactory(art1.name) {
-    repository("maven-local", "maven-virtual") {
-        starPull('Bangkok', 'Cape Town', 'Denver'){
+
+artifactory(sourceArti.name) {
+    def includedRepos = "$repo.value-remote,$repo.value-virtual".split(",")*.trim()
+    repository(includedRepos) {
+        starPull(targetArti.name){
             socketTimeoutMillis 60000
             enableEventReplication true
             cronExp '0 0/9 14 * * ?'
